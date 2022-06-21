14064086bc88d711829cd1d0e45c79eec41d2a04
Redline111111 <handoinilya222@mail.ru>
2022-06-21 07:30:04 +0300

    github pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer(){
     <div class="footer-copyright">
       <div class="container">
       © {new Date().getFullYear()} Copyright Text
-      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      <a class="grey-text text-lighten-4 right" href="https://github.com/redline111111/shop-project" target ="_blank" rel="noreferrer">More Links</a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header(){
     return <nav>
     <div className="nav-wrapper">
-      <a href="#" className="brand-logo">React Shop</a>
+      <a href="/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#">Link</a></li>
+        <li><a href="https://github.com/redline111111/shop-project" target ="_blank" rel="noreferrer">Repo</a></li>
       </ul>
     </div>
   </nav>
